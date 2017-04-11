a0c619ce54494bf4b7eebe3dd0e849f0d8816b15
Samarth Agarwal <samarthagarwal@live.com>
2017-04-11 17:24:45 +0530

    Lec12

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { NavController, Slides } from 'ionic-angular';
+
+import * as WC from 'woocommerce-api';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,36 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  WooCommerce: any;
+  products: any[];
+
+  @ViewChild('productSlides') productSlides: Slides;
+
   constructor(public navCtrl: NavController) {
 
+    this.WooCommerce = WC({
+      url: "http://samarth.cloudapp.net",
+      consumerKey: "ck_b615342c28e3aa9b0b9d384852cda85a82155197",
+      consumerSecret: "cs_d75f28e39ae9f06318608cec44fc77dd75ce6427"
+    });
+
+    this.WooCommerce.getAsync("products").then( (data) => {
+      console.log(JSON.parse(data.body));
+      this.products = JSON.parse(data.body).products;
+    }, (err) => {
+      console.log(err)
+    })
+
+  }
+
+  ionViewDidLoad(){
+    setInterval(()=> {
+
+      if(this.productSlides.getActiveIndex() == this.productSlides.length() -1)
+        this.productSlides.slideTo(0);
+
+      this.productSlides.slideNext();
+    }, 3000)
   }
 
 }
