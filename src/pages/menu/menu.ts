e209e1f2e59c00382fb3f4cc6e91df4f60b39ee1
Samarth Agarwal <samarthagarwal@live.com>
2017-04-17 15:43:48 +0530

    Lec16

--- src/pages/menu/menu.ts
+++ src/pages/menu/menu.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { HomePage } from '../home/home';
+import * as WC from 'woocommerce-api';
 
 @Component({
   selector: 'page-menu',
@@ -9,9 +10,36 @@ import { HomePage } from '../home/home';
 export class Menu {
 
   homePage: Component;
+  WooCommerce: any;
+  categories: any[];
   
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.homePage = HomePage
+    this.categories = [];
+
+     this.WooCommerce = WC({
+      url: "http://samarth.cloudapp.net",
+      consumerKey: "ck_b615342c28e3aa9b0b9d384852cda85a82155197",
+      consumerSecret: "cs_d75f28e39ae9f06318608cec44fc77dd75ce6427"
+    });
+
+
+    this.WooCommerce.getAsync("products/categories").then((data) => {
+      console.log(JSON.parse(data.body).product_categories);
+
+      let temp: any[] = JSON.parse(data.body).product_categories;
+
+      for( let i = 0; i < temp.length; i ++){
+        if(temp[i].parent == 0){
+          this.categories.push(temp[i]);
+        }
+      }
+
+    }, (err)=> {
+      console.log(err)
+    })
+
+
   }
 
   ionViewDidLoad() {
