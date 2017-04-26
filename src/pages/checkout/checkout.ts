2e105f8eeaece744d0bb09a75b4803cc761667e2
Samarth Agarwal <samarthagarwal@live.com>
2017-04-26 23:20:49 +0530

    Lec36

--- src/pages/checkout/checkout.ts
+++ src/pages/checkout/checkout.ts
@@ -7,7 +7,34 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class Checkout {
 
+  newOrder: any;
+  paymentMethods: any[];
+  paymentMethod: any;
+  billing_shipping_same: boolean;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.newOrder = {};
+    this.newOrder.billing_address = {};
+    this.newOrder.shipping_address = {};
+    this.billing_shipping_same = false;
+
+    this.paymentMethods = [
+      { method_id: "bacs", method_title: "Direct Bank Transfer" },
+      { method_id: "cheque", method_title: "Cheque Payment" },
+      { method_id: "cod", method_title: "Cash on Delivery" },
+      { method_id: "paypal", method_title: "PayPal" }];
+
+
+  }
+
+  setBillingToShipping(){
+    this.billing_shipping_same = !this.billing_shipping_same;
+
+    if(this.billing_shipping_same)
+    {
+      this.newOrder.shipping_address = this.newOrder.billing_address;
+    }
+
   }
 
   ionViewDidLoad() {
