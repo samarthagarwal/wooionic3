8962efbeafe3463ae01c15388da4f554c39471b8
Samarth Agarwal <samarthagarwal@live.com>
2017-04-27 01:11:26 +0530

    Lec41

--- src/pages/checkout/checkout.ts
+++ src/pages/checkout/checkout.ts
@@ -18,7 +18,7 @@ export class Checkout {
   billing_shipping_same: boolean;
   userInfo: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public storage: Storage, public alertCtrl: AlertController, public paypal: PayPal) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public storage: Storage, public alertCtrl: AlertController, public payPal: PayPal) {
     this.newOrder = {};
     this.newOrder.billing_address = {};
     this.newOrder.shipping_address = {};
@@ -36,13 +36,13 @@ export class Checkout {
       consumerSecret: "cs_d75f28e39ae9f06318608cec44fc77dd75ce6427"
     });
 
-    this.storage.get("userLoginInfo").then( (userLoginInfo) => {
+    this.storage.get("userLoginInfo").then((userLoginInfo) => {
 
       this.userInfo = userLoginInfo.user;
 
       let email = userLoginInfo.user.email;
 
-      this.WooCommerce.getAsync("customers/email/"+email).then( (data) => {
+      this.WooCommerce.getAsync("customers/email/" + email).then((data) => {
 
         this.newOrder = JSON.parse(data.body).customer;
 
@@ -52,32 +52,31 @@ export class Checkout {
 
   }
 
-  setBillingToShipping(){
+  setBillingToShipping() {
     this.billing_shipping_same = !this.billing_shipping_same;
 
-    if(this.billing_shipping_same)
-    {
+    if (this.billing_shipping_same) {
       this.newOrder.shipping_address = this.newOrder.billing_address;
     }
 
   }
 
-  placeOrder(){
+  placeOrder() {
 
     let orderItems: any[] = [];
     let data: any = {};
 
     let paymentData: any = {};
 
-    this.paymentMethods.forEach( (element, index) => {
-      if(element.method_id == this.paymentMethod){
+    this.paymentMethods.forEach((element, index) => {
+      if (element.method_id == this.paymentMethod) {
         paymentData = element;
       }
     });
 
 
     data = {
-      payment_details : {
+      payment_details: {
         method_id: paymentData.method_id,
         method_title: paymentData.method_title,
         paid: true
@@ -90,14 +89,77 @@ export class Checkout {
     };
 
 
-    if(paymentData.method_id == "paypal"){
-      //TODO
+    if (paymentData.method_id == "paypal") {
+
+      this.payPal.init({
+        PayPalEnvironmentProduction: "YOUR_PRODUCTION_CLIENT_ID",
+        PayPalEnvironmentSandbox: "AYkkS2ObeSpaObaCqA3bybQjRNRMKOw_2vNSha7gmxESpG4l4AhEyMfYwuzrUFKSbWGhCsN-Vhtl5FOG"
+      }).then(() => {
+        // Environments: PayPalEnvironmentNoNetwork, PayPalEnvironmentSandbox, PayPalEnvironmentProduction
+        this.payPal.prepareToRender('PayPalEnvironmentSandbox', new PayPalConfiguration({
+          // Only needed if you get an "Internal Service Error" after PayPal login!
+          //payPalShippingAddressOption: 2 // PayPalShippingAddressOptionPayPal
+        })).then(() => {
+
+          this.storage.get("cart").then((cart) => {
+
+            let total = 0.00;
+            cart.forEach((element, index) => {
+              orderItems.push({ product_id: element.product.id, quantity: element.qty });
+              total = total + (element.product.price * element.qty);
+            });
+
+            let payment = new PayPalPayment(total.toString(), 'USD', 'Description', 'sale');
+            this.payPal.renderSinglePaymentUI(payment).then((response) => {
+              // Successfully paid
+
+              alert(JSON.stringify(response));
+
+
+              data.line_items = orderItems;
+              //console.log(data);
+              let orderData: any = {};
+
+              orderData.order = data;
+
+              this.WooCommerce.postAsync('orders', orderData).then((data) => {
+                alert("Order placed successfully!");
+
+                let response = (JSON.parse(data.body).order);
+
+                this.alertCtrl.create({
+                  title: "Order Placed Successfully",
+                  message: "Your order has been placed successfully. Your order number is " + response.order_number,
+                  buttons: [{
+                    text: "OK",
+                    handler: () => {
+                      this.navCtrl.setRoot(HomePage);
+                    }
+                  }]
+                }).present();
+              })
+
+            })
+
+          }, () => {
+            // Error or render dialog closed without being successful
+          });
+        }, () => {
+          // Error in configuration
+        });
+      }, () => {
+        // Error in initialization, maybe PayPal isn't supported or something else
+      });
+
+
+
+
 
     } else {
 
-      this.storage.get("cart").then( (cart) => {
+      this.storage.get("cart").then((cart) => {
 
-        cart.forEach( (element, index) => {
+        cart.forEach((element, index) => {
           orderItems.push({
             product_id: element.product.id,
             quantity: element.qty
@@ -110,7 +172,7 @@ export class Checkout {
 
         orderData.order = data;
 
-        this.WooCommerce.postAsync("orders", orderData).then( (data) => {
+        this.WooCommerce.postAsync("orders", orderData).then((data) => {
 
           let response = (JSON.parse(data.body).order);
 
