b07097c4d00c48bdd3268897c60560abb10695e7
Samarth Agarwal <samarthagarwal@live.com>
2017-06-30 22:51:23 +0530

    Lec 48

--- src/pages/search/search.ts
+++ src/pages/search/search.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-
+import * as WC from 'woocommerce-api';
 
 @Component({
   selector: 'page-search',
@@ -9,9 +9,24 @@ import { NavController, NavParams } from 'ionic-angular';
 export class SearchPage {
 
   searchQuery: string = "";
+  WooCommerce: any;
+  products: any[] = [];
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     console.log(this.navParams.get("searchQuery"));
+    this.searchQuery = this.navParams.get("searchQuery");
+
+    this.WooCommerce = WC({
+      url: "http://samarth.cloudapp.net",
+      consumerKey: "ck_d6c5feec9ea1c407d2f91661c5137c6e3e48ae3b",
+      consumerSecret: "cs_de8e6cf03a5afd10491dfb1756415ac5a0169ae8"
+    });
+
+    this.WooCommerce.getAsync("products?filter[q]=" + this.searchQuery).then((searchData) => {
+      this.products = JSON.parse(searchData.body).products;
+    });
+
+
   }
 
   ionViewDidLoad() {
