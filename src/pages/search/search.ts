1244bec2c006c4bbcca8caf141a2a5920b66d69b
Samarth Agarwal <samarthagarwal@live.com>
2017-07-03 20:17:14 +0530

    Lec47

--- src/pages/search/search.ts
+++ src/pages/search/search.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams, ToastController } from 'ionic-angular';
-import * as WC from 'woocommerce-api';
+import { NavController, NavParams} from 'ionic-angular';
 
 @Component({
   selector: 'page-search',
@@ -9,50 +8,14 @@ import * as WC from 'woocommerce-api';
 export class SearchPage {
 
   searchQuery: string = "";
-  WooCommerce: any;
-  products: any[] = [];
-  page: number = 2;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public toastCtrl: ToastController) {
     console.log(this.navParams.get("searchQuery"));
-    this.searchQuery = this.navParams.get("searchQuery");
-
-    this.WooCommerce = WC({
-      url: "http://samarth.cloudapp.net",
-      consumerKey: "ck_d6c5feec9ea1c407d2f91661c5137c6e3e48ae3b",
-      consumerSecret: "cs_de8e6cf03a5afd10491dfb1756415ac5a0169ae8"
-    });
-
-    this.WooCommerce.getAsync("products?filter[q]=" + this.searchQuery).then((searchData) => {
-      this.products = JSON.parse(searchData.body).products;
-    });
-
-
+    
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SearchPage');
-  }
-
-  loadMoreProducts(event){
-
-    this.WooCommerce.getAsync("products?filter[q]=" + this.searchQuery + "&page=" + this.page).then((searchData) => {
-      this.products = this.products.concat(JSON.parse(searchData.body).products);
-
-      if(JSON.parse(searchData.body).products.length < 10){
-        event.enable(false);
-
-        this.toastCtrl.create({
-          message: "No more products!",
-          duration: 5000
-        }).present();
-
-      }
-
-      event.complete();
-      this.page ++;
-
-    });
   }
 
 }
