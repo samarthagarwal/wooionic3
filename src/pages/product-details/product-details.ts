94793f0ea7a9c9043eba8c7429a07c8126c29a39
Samarth Agarwal <samarthagarwal@live.com>
2018-03-26 23:47:12 +0530

    Lec62-Updating addToCart for Variations

--- src/pages/product-details/product-details.ts
+++ src/pages/product-details/product-details.ts
@@ -42,43 +42,98 @@ export class ProductDetails {
 
   addToCart(product) {
 
+    //counting selected attribute options
+    let count = 0;
+    for (let k in this.selectedOptions) if (this.selectedOptions.hasOwnProperty(k)) count++;
+
+    //counting variation attributes options
+    let count_ = 0;
+    for (var index = 0; index < this.product.attributes.length; index++) {
+      
+      if(this.product.attributes[index].variation)
+        count_++;
+      
+    }
+
+    //checking if user selected all the variation options or not
+
+    if(count_ != count || this.requireOptions)
+    {
+      this.toastCtrl.create({
+        message: "Select Product Options",
+        duration: 2000,
+        showCloseButton: true
+      }).present();
+      return; 
+    }
+
+
+
     this.storage.get("cart").then((data) => {
 
-      if (data == null || data.length == 0) {
+      if (data == undefined || data.length == 0) {
         data = [];
 
         data.push({
           "product": product,
           "qty": 1,
           "amount": parseFloat(product.price)
-        })
-      } else {
+        });
 
-        let added = 0;
-
-        for (let i = 0; i < data.length; i++) {
-
-          if (product.id == data[i].product.id) {
-            let qty = data[i].qty;
-
-            console.log("Product is already in the cart");
-
-            data[i].qty = qty + 1;
-            data[i].amount = parseFloat(data[i].amount) + parseFloat(data[i].product.price);
-            added = 1;
-          }
-
+        if(this.selectedVariation){
+          data[0].variation = this.selectedVariation;
+          data[0].amount = parseFloat(this.selectedVariation.price);
         }
 
-        if (added == 0) {
-          data.push({
-            "product": product,
-            "qty": 1,
-            "amount": parseFloat(product.price)
-          })
+      } else {
+
+        let alreadyAdded = false;
+        let alreadyAddedIndex = -1;
+
+        for (let i = 0; i < data.length; i++){
+          if(data[i].product.id == product.id){ //Product ID matched
+            if(this.productVariations.length > 0){ //Now match variation ID also if it exists
+              if(data[i].variation.id == this.selectedVariation.id){
+                alreadyAdded = true;
+                alreadyAddedIndex = i;
+                break;
+              }
+            } else { //product is simple product so variation does not  matter
+              alreadyAdded = true;
+              alreadyAddedIndex = i;
+              break;
+            }
+          }
+        }
+
+        if(alreadyAdded == true){
+          if(this.selectedVariation){
+            data[alreadyAddedIndex].qty = parseFloat(data[alreadyAddedIndex].qty) + 1;
+            data[alreadyAddedIndex].amount = parseFloat(data[alreadyAddedIndex].amount) + parseFloat(this.selectedVariation.price);
+            data[alreadyAddedIndex].variation = this.selectedVariation;
+          } else {
+            data[alreadyAddedIndex].qty = parseFloat(data[alreadyAddedIndex].qty) + 1;
+            data[alreadyAddedIndex].amount = parseFloat(data[alreadyAddedIndex].amount) + parseFloat(data[alreadyAddedIndex].product.price);
+          } 
+        } else {
+          if(this.selectedVariation){
+            data.push({
+              product: product,
+              qty: 1,
+              amount: parseFloat(this.selectedVariation.price),
+              variation: this.selectedVariation
+            })
+          } else {
+            data.push({
+              product: product,
+              qty: 1,
+              amount: parseFloat(product.price)
+            })
+          }
         }
 
       }
+
 
       this.storage.set("cart", data).then(() => {
         console.log("Cart Updated");
